303b405429ac42ea42315ec397078bb17661561d
Naruto <tranviethung912002@gmail.com>
2023-02-16 02:36:01 +0700

    update

--- src/modules/AminPage/components/ProductList/ProductList.js
+++ src/modules/AminPage/components/ProductList/ProductList.js
@@ -12,63 +12,160 @@ import { useLocation } from 'react-router-dom';
 import Spinner from '~/shared/components/spinner/spinner';
 import PageNumber from '~/modules/Products/PageNumber/PageNumber';
 import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  setCurrentPage,
+  setNumbersPage,
+  setSearchValue,
+  setSortFilter,
+  setTotalPage,
+} from '~/redux/filter/filter.action';
+import NotFoundProduct from '~/modules/Products/NotFoundProduct/NotFoundProduct';
 
 const cx = classNames.bind(style);
 
 function ProductList() {
   const { product, loading, error } = useSelector((state) => state.product);
+  const { numbersPage, currentPage, totalPage, sortFilter, priceRange, searchValue } = useSelector(
+    (state) => state.filter,
+  );
+  const [category, setCategory] = useState(categoryList[0].path);
   const dispatch = useDispatch();
-  const { pathname } = useLocation();
-  const [numbersPage, setNumbersPage] = useState([1]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPage, setTotalPage] = useState(1);
-  const [sortFilter, setSortFilter] = useState(sortFilterList[0]);
 
   useEffect(() => {
-    dispatch(loadProduct());
-  }, [pathname]);
+    dispatch(loadProduct(category.slice(10), currentPage, sortFilter, priceRange, searchValue));
+  }, [category, currentPage, sortFilter, priceRange, searchValue]);
+
+  useEffect(() => {
+    if (product) {
+      let total;
+      total = Math.ceil(product.total / 30);
+      if (Number.isNaN(total)) dispatch(setTotalPage(1));
+      else dispatch(setTotalPage(total));
+    }
+  }, [product]);
+
+  useEffect(() => {
+    let arr = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i > totalPage) break;
+      else arr.push(i);
+    }
+    dispatch(setNumbersPage(arr));
+  }, [totalPage]);
+
+  useEffect(() => {
+    let arr = [];
+    if (totalPage <= 5) return;
+
+    if (currentPage >= numbersPage[numbersPage.length - 1] && currentPage < totalPage) {
+      if (totalPage - currentPage >= 2) {
+        for (let i = currentPage - 2; i <= currentPage + 2; i++) {
+          arr.push(i);
+        }
+      } else {
+        for (let i = totalPage - 4; i <= totalPage; i++) {
+          arr.push(i);
+        }
+      }
+      dispatch(setNumbersPage(arr));
+    } else if (currentPage <= numbersPage[0] && currentPage < totalPage) {
+      if (currentPage > 2) {
+        for (let i = currentPage - 2; i <= currentPage + 2; i++) {
+          arr.push(i);
+        }
+      } else {
+        for (let i = 1; i <= 5; i++) {
+          arr.push(i);
+        }
+      }
+      dispatch(setNumbersPage(arr));
+    }
+  }, [currentPage]);
+
+  const [pageValue, setPageValue] = useState(currentPage);
+  const [inputWidth, setInputWidth] = useState(12);
+  useEffect(() => {
+    setPageValue(currentPage);
+  }, [currentPage]);
+
+  useEffect(() => {
+    if (!Number.isNaN(Number.parseInt(pageValue)))
+      dispatch(setCurrentPage(Number.parseInt(pageValue)));
+    if (pageValue.length > 0) setInputWidth(12 + 9 * (pageValue.length - 1));
+  }, [pageValue]);
 
   return (
     <div className={cx('wrapper')}>
       <div className={cx('search')}>
         <span>Tìm kiếm:</span>
-        <input placeholder="Nhập mã sản phẩm hoặc tên sản phẩm để tìm kiếm..." />
+        <input
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              dispatch(setSearchValue(e.target.value));
+              e.target.value = '';
+            }
+          }}
+          placeholder="Nhập mã sản phẩm hoặc tên sản phẩm để tìm kiếm..."
+        />
       </div>
 
       <div className={cx('wrap-line')}>
         <span className={cx('filter')}>
           <span>Danh mục:</span>
-          <select>
+          <select onChange={(e) => setCategory(e.target.value)}>
             {categoryList.map((category, index) => (
-              <option key={index}>{category.viewValue}</option>
+              <option value={category.path} key={index}>
+                {category.viewValue}
+              </option>
             ))}
           </select>
           <span>Sắp xếp:</span>
-          <select>
+          <select
+            onChange={(e) => {
+              dispatch(
+                setSortFilter(sortFilterList.find((filter) => filter.label === e.target.value)),
+              );
+            }}
+          >
             {sortFilterList.map((filter, index) => (
-              <option key={index}>{filter.label}</option>
+              <option value={filter.label} key={index}>
+                {filter.label}
+              </option>
             ))}
           </select>
         </span>
 
         <div className={cx('page')}>
-          <PageNumber>
+          <span className={cx('page-label')}>
+            Trang{' '}
+            <input
+              className={cx('page-input')}
+              value={pageValue}
+              style={{ width: inputWidth }}
+              onChange={(e) => {
+                let tmp = e.target.value;
+                let lastChar = tmp[tmp.length - 1];
+                if (
+                  tmp === '' ||
+                  (tmp[0] !== '0' &&
+                    lastChar <= '9' &&
+                    lastChar >= '0' &&
+                    Number.parseInt(tmp) <= totalPage)
+                )
+                  setPageValue(tmp);
+              }}
+            />
+            / {totalPage}
+          </span>
+          <PageNumber noneMargin={true}>
             <FontAwesomeIcon
-              onClick={() =>
-                setCurrentPage((prev) => {
-                  return prev > 1 ? prev - 1 : prev;
-                })
-              }
+              onClick={() => dispatch(setCurrentPage(currentPage - 1))}
               icon={faCaretLeft}
             />
           </PageNumber>
-          <PageNumber>
+          <PageNumber noneMargin={true}>
             <FontAwesomeIcon
-              // onClick={() =>
-              // setCurrentPage((prev) => {
-              //   return prev < totalPage ? prev + 1 : prev;
-              // })
-              // }
+              onClick={() => dispatch(setCurrentPage(currentPage + 1))}
               icon={faCaretRight}
             />
           </PageNumber>
@@ -92,6 +189,7 @@ function ProductList() {
 
         {loading && <Spinner />}
         {error && <h3>Error</h3>}
+        {!totalPage && <NotFoundProduct />}
         {product &&
           product.products &&
           product.products.length > 0 &&
@@ -99,32 +197,18 @@ function ProductList() {
         <div className={cx('page')}>
           <PageNumber>
             <FontAwesomeIcon
-              onClick={() =>
-                setCurrentPage((prev) => {
-                  return prev > 1 ? prev - 1 : prev;
-                })
-              }
+              onClick={() => dispatch(setCurrentPage(currentPage - 1))}
               icon={faCaretLeft}
             />
           </PageNumber>
+
           {numbersPage.map((numberPage) => {
-            return (
-              <PageNumber
-                key={numberPage}
-                currentPage={currentPage}
-                // onClick={() => setCurrentPage(numberPage)}
-              >
-                {numberPage}
-              </PageNumber>
-            );
+            return <PageNumber key={numberPage} numberPage={numberPage} />;
           })}
+
           <PageNumber>
             <FontAwesomeIcon
-              // onClick={() =>
-              // setCurrentPage((prev) => {
-              //   return prev < totalPage ? prev + 1 : prev;
-              // })
-              // }
+              onClick={() => dispatch(setCurrentPage(currentPage + 1))}
               icon={faCaretRight}
             />
           </PageNumber>
