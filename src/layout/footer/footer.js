22abd232a86d34e6eb75115746393c1cf9ddce70
TrangNL <linhtrang1602.hust@gmail.com>
2023-02-17 00:23:47 +0700

    Update Footer

--- src/layout/footer/footer.js
+++ src/layout/footer/footer.js
@@ -8,18 +8,18 @@ function Footer() {
         <div className="row no-gutter">
           <div className="col text-start l-3 m-6 c-6">
             <h4>Địa chỉ</h4>
-            <p>Something</p>
-            <p>Something</p>
+            <p>Số 1 Đại Cồ Việt, Hai Bà Trưng, Hà Nội</p>
+            {/* <p>Something</p> */}
           </div>
           <div className="col text-start l-3 m-6 c-6">
             <h4>Liên hệ</h4>
-            <p>Something</p>
-            <p>Something</p>
+            <p>Hotline: (84)112 334 567</p>
+            <p>Email: candleofthewind@gmail.com</p>
           </div>
           <div className="col text-start l-3 m-6 c-6">
             <h4>Đơn vị vận chuyển</h4>
-            <p>Something</p>
-            <p>Something</p>
+            {/* <p>Something</p> */}
+            <p>Giao hàng tiết kiệm</p>
           </div>
         </div>
       </div>
